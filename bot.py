0cb8667709f7e2005411186edf7bd1844a0d2caf
sagi67-spec <ikernicolastorres12@gmail.com>
2026-08-04 15:53:29 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -495,6 +495,8 @@ async def generar_licencia(usuario: discord.Member, datos_licencia: dict):
             font_estado = ImageFont.truetype("fonts/Montserrat-Bold.ttf", 34)
             font_lic = ImageFont.truetype("fonts/Montserrat-Bold.ttf", 26)
             font_marca = ImageFont.truetype("fonts/Montserrat-Bold.ttf", 80)
+        except:
+            font_title = font_sub = font_label = font_value = font_small = font_micro = font_estado = font_lic = font_marca = ImageFont.load_default()
 < truncated lines 185-212 >
         tw, th = tile.size
 
